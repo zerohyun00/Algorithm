4432739bcf75351e9177e171329fbf86e152b0d6
Younghyun Kim <113604373+zerohyun00@users.noreply.github.com>
2024-08-22 22:15:18 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.56 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+  let result = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      result.push(numbers[i] + numbers[j]);
+    }
+  }
+  return [...new Set(result)].sort((a, b) => a - b);
+}
