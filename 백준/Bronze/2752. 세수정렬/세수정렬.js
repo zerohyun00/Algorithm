e96d3f09e254f912fe122d9592758f1c089bc10e
Younghyun Kim <113604373+zerohyun00@users.noreply.github.com>
2024-08-30 22:28:08 +0900

    [Bronze IV] Title: 세수정렬, Time: 96 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.js
@@ -0,0 +1,12 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let arr = input[0].split(' ').map(Number);
+arr.sort((a, b) => a - b);
+
+let answer = '';
+for (let i = 0; i < arr.length; i++) {
+  answer += arr[i] + ' ';
+}
+console.log(answer);
+
